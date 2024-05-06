8b00b12bf42a848af51220e9664e9fbff732ee40
vedashreeMPatil <vedashreepatil07@gmail.com>
2024-05-06 18:20:42 +0530

    add file

---
+++ Methods.java
@@ -0,0 +1,22 @@
+public class methods {
+ 
+    void mul(double num, int num2)
+    {
+        double r=num*num2;
+        System.out.println(r);
+    }
+    void mul(float num1, int num2)
+    {
+        float r1= num1* num2;
+        System.out.println(r1);
+
+
+}
+    public static void main (string args[]) {
+        methods x=new methods();
+        x.mul(10.5, 0);
+        methods x= new methods();
+        x1.mul(10.5f,)
+    }
+
+}
